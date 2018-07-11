9e6e76e957365b29b19b0f18cbab3946570c40f7
yochp <peled770@gmail.com>
2018-07-11 19:05:02 +0300

    haeder add logo and title

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,13 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-//import searchImg from '../../assets/images/search.png';
 
 class Search extends Component {
 
   constructor(props) {
     super(props);
 
-    // This binding is necessary to make `this` work in the callback
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  // onSubmit(id, e) {
-  //   e.preventDefault();
-  //   const selected = this.props.options.find(option => option.id === id);
-  //   this.props.selected(selected);
-  //   this.props.active(this.props.current);
-  // }
 
   onSubmit(e) {
     e.preventDefault();
@@ -25,12 +16,16 @@ class Search extends Component {
     this.myRef.value = '';
   }
 
+
+
+
+
   render() {
     return (
       <div>
       <form onSubmit={this.onSubmit.bind(this)}>
-        <input ref={ref => this.myRef = ref}/>
-        <button>
+        <input className={"inputSearch"}  placeholder={"חפש מה שבא לך"} ref={ref => this.myRef = ref}/>
+        <button >
           <img src={'assets/images/search.png'} alt={'Search'} width="16px" height="16px"/>
         </button>
       </form>
