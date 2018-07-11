8169d2c59654bbb9d32bee768a9c5872363e99f6
yochp <peled770@gmail.com>
2018-07-11 19:44:40 +0300

    haeder add logo and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Search from './Search';
 
 const Header = () => (
- <div class="App-header">
+    <div className="App-header">
 
-<img class="App-logo" src="../../assets/images/oknesset-logo.png"/>
-     <span class="title">כנסת פתוחה</span>
-   <Search/>
- </div>
+        <img className="App-logo" src={"assets/images/oknesset-logo.png"}/>
+        <span className="title">כנסת פתוחה</span>
+        <Search/>
+    </div>
 );
-
 export default Header;
