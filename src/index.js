0bd85b36d45b898db53621c4f0fcc3f6c3060418
yuvalbl <yuval.bar.levi@gmail.com>
2018-07-11 20:38:52 +0300

    add font awesome

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faUsers);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
