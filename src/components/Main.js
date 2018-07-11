a2d8aaffb0693688d7cb159ae6c382d7691f73d1
Roy Nevo <36882185+TheBigR@users.noreply.github.com>
2018-07-11 17:18:37 +0300

    added mock main block

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-    const mock=[
-            {title: ,
-            body: ,
-            },
-            {},
-            {},
-            {}
-        ]
+
   <main>
       <TopicBlock></TopicBlock>
   </main>
---
+++ src/components/TopicsMock.js
@@ -0,0 +1,25 @@
+const mock=[
+    {topicCategoryTitle: "Committees",
+        topicTitle: "Sample Title 1",
+        topicSubtitle: "just some crap to see this works",
+        topicUrl: "fakeUrl"},
+    {topicCategoryTitle: "Committees",
+        topicTitle: "Sample Title 2",
+        topicSubtitle: "just some crap to see this works",
+        topicUrl: "fakeUrl"},
+    {topicCategoryTitle: "Committees",
+        topicTitle: "Sample Title 3",
+        topicSubtitle: "just some crap to see this works",
+        topicUrl: "fakeUrl"},
+    {topicCategoryTitle: "Committees",
+        topicTitle: "Sample Title 1",
+        topicSubtitle: "just some crap to see this works",
+        topicUrl: "fakeUrl"}
+];
+
+const categoryDictionary = [
+    {topicCategoryTitle: "Committees", categoryIcon: "fakeIconUrl", categoryDisplayName: 'ועדות'},
+    {topicCategoryTitle: "Ruling", categoryIcon: "fakeIconUrl", categoryDisplayName: 'חקיקה'},
+    {topicCategoryTitle: "Votes", categoryIcon: "fakeIconUrl", categoryDisplayName: 'הצבעות'},
+    {topicCategoryTitle: "Assembly", categoryIcon: "fakeIconUrl", categoryDisplayName: 'מליאה'}
+];
