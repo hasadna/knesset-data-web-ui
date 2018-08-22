c18389235b6eb885ad49511be58e349abc0d6c8d
yuvalbl <yuval.bar.levi@gmail.com>
2018-08-22 13:29:54 +0300

    add basic styles refactor components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Search from './Search';
-import {NavLink, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
+import NavBar from './NavBar';
+import BreadCrumb from './BreadCrumb';
 
 const Header = () => (
-	<div className="App-header">
+	<header className="App-header">
 		<Link to="/">
 			<img className="App-logo" src={'assets/images/oknesset-logo.png'}/>
 		</Link>
 
 		<span className="title">כנסת פתוחה</span>
 		<Search/>
-		<NavLink to="/members" activeClassName="selected">members</NavLink>
-		<NavLink to="/committees" activeClassName="selected">committees</NavLink>
-	</div>
+		<NavBar/>
+		<BreadCrumb/>
+	</header>
 );
 export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import './NavBar.css';
+
+
+const NavBar = () => (
+	<ul className="NavBar nav">
+		<li className="nav-item">
+			<NavLink to="/members" activeClassName="selected">ח"כים וסיעות</NavLink>
+		</li>
+		<li className="nav-item">
+			<NavLink to="/committees" activeClassName="selected">ועדות</NavLink>
+		</li>
+		<li className="nav-item">
+			<NavLink to="/" activeClassName="selected">bla</NavLink>
+		</li>
+		<li className="nav-item">
+			<NavLink to="/" activeClassName="selected">bla</NavLink>
+		</li>
+		<li className="nav-item">
+			<NavLink to="/" activeClassName="selected">bla</NavLink>
+		</li>
+		<li className="nav-item">
+			<NavLink to="/" activeClassName="selected">bla</NavLink>
+		</li>
+	</ul>
+);
+
+export default NavBar;
